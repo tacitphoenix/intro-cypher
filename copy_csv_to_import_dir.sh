4c0cd3658a20523ababa3af7b2913d2291de34d1
tacitphoenix <sondiek@gmail.com>
2019-11-01 12:24:23 -0400

    display files to be copied and files in import directory

--- copy_csv_to_import_dir.sh
+++ copy_csv_to_import_dir.sh
@@ -6,8 +6,16 @@
 # set import directory to variable
 NEO4J_IMPORT_DIR=`grep -o '/.*t' ./output`
 
+# files to be copied
+echo "Files to be copied"
+ls -l ./data|cat
+
 # copy csv file in data directory to neo4j import directory
 cp ./data/* "$NEO4J_IMPORT_DIR" 
 #echo $NEO4J_IMPORT_DIR 
+
+#files in import directory
+echo "Files in import directory"
+ls -l "$NEO4J_IMPORT_DIR"|cat
 
 
